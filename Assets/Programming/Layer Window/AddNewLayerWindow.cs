d93185910bcfa0bb44845712fe758d3f68dda8cc
Shadow <blenminer123@gmail.com>
2022-06-01 10:21:33 +0200

    formula prefab

--- Assets/Programming/Layer Window/AddNewLayerWindow.cs
+++ Assets/Programming/Layer Window/AddNewLayerWindow.cs
@@ -111,7 +111,7 @@ public class MapLayer
     public Color MinColor, MaxColor;
 
     public MapType Type;
-    
+
     public MapLayerRaw ToRawLayer()
     {
         return new MapLayerRaw()
@@ -141,7 +141,7 @@ public class AddNewLayerWindow : WindowBehaviour
         Type enumType = typeof(MapType);
         Array enumValues = Enum.GetValues(enumType);
 
-        for (int i=0; i < enumValues.Length; i++)
+        for (int i = 0; i < enumValues.Length; i++)
         {
             // Retrieve the value of the ith enum item.
             string value = enumValues.GetValue(i).ToString();
@@ -160,8 +160,9 @@ public class AddNewLayerWindow : WindowBehaviour
         m_startColor.color = input.MinColor;
         m_endColor.color = input.MaxColor;
         m_name.text = input.Name;
-        m_formulaInput.text = input.Formula.RawText;
-        m_sizeFormulaInput.text = input.SizeFormula.RawText;
+
+        m_valueFormulaUI.SetText(input.Formula.RawText);
+        m_sizeFormulaUI.SetText(input.SizeFormula.RawText);
 
         m_onSubmit = layer;
     }
@@ -180,44 +181,24 @@ public class AddNewLayerWindow : WindowBehaviour
 
     [Header("Formula Settings")]
 
-    [SerializeField] TMP_Text m_errorText;
+    [SerializeField] FormulaUI m_valueFormulaUI;
 
-    [SerializeField] TMP_Text m_minText;
-
-    [SerializeField] TMP_Text m_maxText;
-
-    [SerializeField] TMP_InputField m_formulaInput;
-
-    [SerializeField] Rectangle m_forumlaBox;
-
-    [SerializeField] Color m_validForumlaColor = Color.green, m_invalidForumlaColor = Color.red;
-
-    [Header("Size Formula Settings")]
-
-    [SerializeField] TMP_Text m_sizeErrorText;
-
-    [SerializeField] TMP_Text m_sizeMinText;
-
-    [SerializeField] TMP_Text m_sizeMaxText;
-
-    [SerializeField] TMP_InputField m_sizeFormulaInput;
-
-    [SerializeField] Rectangle m_sizeForumlaBox;
+    [SerializeField] FormulaUI m_sizeFormulaUI;
 
     [Header("Formula Colors")]
 
     [SerializeField] Graphic m_startColor;
-    
+
     [SerializeField] Graphic m_endColor;
- 
+
     public static string ToKMB(float num)
     {
-        if (num > 999999999 || num < -999999999 )
+        if (num > 999999999 || num < -999999999)
         {
             return num.ToString("0,,,.###B", CultureInfo.InvariantCulture);
         }
         else
-        if (num > 999999 || num < -999999 )
+        if (num > 999999 || num < -999999)
         {
             return num.ToString("0,,.##M", CultureInfo.InvariantCulture);
         }
@@ -232,57 +213,6 @@ public class AddNewLayerWindow : WindowBehaviour
         }
     }
 
-    private void OnEnable()
-    {
-        m_formulaInput.onValueChanged
-            .AddListener(OnFormulaUpdated);
-
-        m_sizeFormulaInput.onValueChanged
-            .AddListener(OnSizeFormulaUpdated);
-        
-        //m_timeMachine.OnTimeMachineUpdate += OnTimeUpdated;
-    }
-
-    private void OnTimeUpdated(float _)
-    {
-        if (m_formula != null && m_formula.IsValid)
-        {
-            if (m_formula.ComputeMin(out var min))
-                 m_minText.text = $"<color=grey>Min Value</color> {ToKMB(min)}";
-            else m_minText.text = "<color=grey>Min Value</color> -";
-
-            if (m_formula.ComputeMax(out var max))
-                 m_maxText.text = $"{ToKMB(max)} <color=grey>Max Value</color>";
-            else m_maxText.text = "- <color=grey>Max Value</color>";
-        }
-
-        if (m_sizeFormula != null && m_sizeFormula.IsValid)
-        {
-            if (m_sizeFormula.ComputeMin(out var min))
-                 m_sizeMinText.text = $"<color=grey>Min Value</color> {ToKMB(min)}";
-            else m_sizeMinText.text = "<color=grey>Min Value</color> -";
-
-            if (m_sizeFormula.ComputeMax(out var max))
-                 m_sizeMaxText.text = $"{ToKMB(max)} <color=grey>Max Value</color>";
-            else m_sizeMaxText.text = "- <color=grey>Max Value</color>";
-        }
-    }
-
-    private void Start()
-    {
-        OnFormulaUpdated(m_formulaInput.text);
-        OnSizeFormulaUpdated(m_sizeFormulaInput.text);
-    }
-
-    private void OnDisable()
-    {
-        m_formulaInput.onValueChanged
-            .RemoveListener(OnFormulaUpdated);
-
-        m_sizeFormulaInput.onValueChanged
-            .RemoveListener(OnSizeFormulaUpdated);
-    }
-
     public void Submit()
     {
         Type enumType = typeof(MapType);
@@ -291,62 +221,17 @@ public class AddNewLayerWindow : WindowBehaviour
 
         MapType type = (MapType)enumValues.GetValue(m_type.value);
 
-        m_onSubmit?.Invoke(new MapLayer {
+        m_onSubmit?.Invoke(new MapLayer
+        {
             Name = m_name.text,
-            Formula = m_formula,
-            SizeFormula = m_sizeFormula,
+            Formula = m_valueFormulaUI.Formula,
+            SizeFormula = m_sizeFormulaUI.Formula,
             MaxColor = m_endColor.color,
             MinColor = m_startColor.color,
             Type = type
         });
 
         ForcePopWindow();
-    }
-
-    Formula m_formula;
-
-    Formula m_sizeFormula;
-
-    bool UpdateFormula(string value)
-    {
-        m_formula = new Formula(m_autoCompletionProvider, value);
-        return m_formula.IsValid;
-    }
-
-    bool UpdateSizeFormula(string value)
-    {
-        m_sizeFormula = new Formula(m_autoCompletionProvider, value, "1");
-        return m_sizeFormula.IsValid;
-    }
-
-    void OnFormulaUpdated(string value)
-    {
-        bool validSyntax = UpdateFormula(value);
-
-        m_forumlaBox.ShapeProperties.OutlineColor =
-            validSyntax ? m_validForumlaColor : m_invalidForumlaColor;
-
-        if (!validSyntax) m_errorText.text = m_formula.Error;
-        else m_errorText.text = string.Empty;
-        
-        m_forumlaBox.SetAllDirty();
-
-        if (validSyntax) OnTimeUpdated(m_timeMachine.CurrentPercentage);
-    }
-
-    void OnSizeFormulaUpdated(string value)
-    {
-        bool validSyntax = UpdateSizeFormula(value);
-
-        m_sizeForumlaBox.ShapeProperties.OutlineColor =
-            validSyntax ? m_validForumlaColor : m_invalidForumlaColor;
-
-        if (!validSyntax) m_sizeErrorText.text = m_formula.Error;
-        else m_sizeErrorText.text = string.Empty;
-        
-        m_sizeForumlaBox.SetAllDirty();
-
-        if (validSyntax) OnTimeUpdated(m_timeMachine.CurrentPercentage);
     }
 
     public void ChangeStartColor()
---
+++ Assets/Programming/Layer Window/FormulaUI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using ThisOtherThing.UI.Shapes;
+using TMPro;
+using UnityEngine;
+
+public class FormulaUI : MonoBehaviour
+{
+    [SerializeField] TMP_Text m_errorText;
+
+    [SerializeField] TMP_Text m_minText;
+
+    [SerializeField] TMP_Text m_maxText;
+
+    [SerializeField] TMP_InputField m_formulaInput;
+
+    [SerializeField] Rectangle m_forumlaBox;
+
+    [SerializeField] Color m_validForumlaColor = Color.green, m_invalidForumlaColor = Color.red;
+
+    [Inject] DatasetAutocompletion m_autoCompletionProvider;
+
+    public Formula Formula { get; private set; }
+
+    void OnEnable()
+    {
+        if (Formula == null)
+            Formula = new Formula(m_autoCompletionProvider, "", "1");
+        m_formulaInput.onValueChanged
+            .AddListener(OnFormulaUpdated);
+    }
+
+    void OnDisable()
+    {
+        m_formulaInput.onValueChanged
+            .RemoveListener(OnFormulaUpdated);
+    }
+
+    void OnFormulaUpdated(string value)
+    {
+        UpdateFormula(value);
+    }
+
+    public void SetText(string value)
+    {
+        m_formulaInput.text = value;
+        UpdateFormula(value);
+    }
+
+    bool UpdateFormula(string value)
+    {
+        Formula = new Formula(m_autoCompletionProvider, value, "1");
+        bool validSyntax = Formula.IsValid;
+
+        m_forumlaBox.ShapeProperties.OutlineColor =
+            validSyntax ? m_validForumlaColor : m_invalidForumlaColor;
+
+        if (!validSyntax) m_errorText.text = Formula.Error;
+        else m_errorText.text = string.Empty;
+
+        m_forumlaBox.SetAllDirty();
+
+        UpdateMinMaxValue();
+
+        return validSyntax;
+    }
+
+    private void UpdateMinMaxValue()
+    {
+        if (Formula != null && Formula.IsValid)
+        {
+            if (Formula.ComputeMin(out var min))
+                m_minText.text = $"<color=grey>Min Value</color> {AddNewLayerWindow.ToKMB(min)}";
+            else m_minText.text = "<color=grey>Min Value</color> -";
+
+            if (Formula.ComputeMax(out var max))
+                m_maxText.text = $"{AddNewLayerWindow.ToKMB(max)} <color=grey>Max Value</color>";
+            else m_maxText.text = "- <color=grey>Max Value</color>";
+        }
+    }
+}
